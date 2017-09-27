20b8b6ea0ca7e7be1899e6b07215883a03147ee6
Anthony Morgano <anthonymorgano@Anthony-Morganos-MacBook-Pro.local>
2017-09-27 12:46:27 -0400

    Initial Commit

---
+++ SavetheKittensTests/SavetheKittensTests.swift
@@ -0,0 +1,36 @@
+//
+//  SavetheKittensTests.swift
+//  SavetheKittensTests
+//
+//  Created by Anthony Morgano on 9/27/17.
+//  Copyright © 2017 Anthony Morgano. All rights reserved.
+//
+
+import XCTest
+@testable import SavetheKittens
+
+class SavetheKittensTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
